dd8c72639a7e0eaea5279cdcc56a67323c234974
Greg Bromage <greg@enimga-it.com.au>
2014-10-27 06:00:58 +0000

    Added additional commands

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -2,13 +2,24 @@ from fabric.api import *
 
 
 env.use_ssh_config = True
+env.hosts = open('hosts.list', 'r').readlines()
 
-env.hosts = [''] # List of hosts
-
-def ban(jail, ip)
+def ban(jail, ip='null'):
     """ Bans a specific IP address """
+    run("sudo /usr/bin/fail2ban-client set %s banip %s" % (jail, ip))
 
-def unban(jail, ip)
+def unban(jail, ip='null'):
     """ Unban an IP address """
+    run("sudo /usr/bin/fail2ban-client set %s unbanip %s" % (jail, ip))
 
+def whitelist(jail, ip='null'):
+    """ Add IP to Whitelist """
+    run("sudo /usr/bin/fail2ban-client set %s addignoreip %s" % (jail, ip))
 
+def unwhitelist(jail, ip='null'):
+    """ Remote IP from whitelist """
+    run("sudo /usr/bin/fail2ban-client set %s delignoreip %s" % (jail, ip))
+
+def status(jail=''):
+    """ Gets the status of a jail """
+    run("sudo /usr/bin/fail2ban-client status %s" % jail)
