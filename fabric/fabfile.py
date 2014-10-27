062efbf392700f85a9d4a8effe7fda8114072f06
Greg Bromage <greg@enimga-it.com.au>
2014-10-27 05:49:30 +0000

    Initial commit  - Added FabFile

---
+++ fabric/fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import *
+
+
+env.use_ssh_config = True
+
+env.hosts = [''] # List of hosts
+
+def ban(jail, ip)
+    """ Bans a specific IP address """
+
+def unban(jail, ip)
+    """ Unban an IP address """
+
+
